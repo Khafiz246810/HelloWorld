49a901e474551c05625d718cc40d1a28b5a4872b
Khafiz <116889143+Khafiz246810@users.noreply.github.com>
2023-01-22 13:46:10 +0600

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class HelloWorld 
+{
+  public static void main(String[] args)
+  {
+    String hello = "Hello World";
+    System.out.printIn(sayHello());
+  }
+  
+  public static String sayHello()
+  {
+    return "Hello World!";
+  }
+}
